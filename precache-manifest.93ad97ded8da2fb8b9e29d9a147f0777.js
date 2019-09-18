04886943fddab654f8d5dd94a0b95b8e02f044c1
Michael Stuhlman <mstuhlman@configureone.com>
2019-09-18 15:20:16 -0500

    Updates

---
+++ precache-manifest.93ad97ded8da2fb8b9e29d9a147f0777.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d82902a72cf6396e84decca85edd37f",
+    "url": "/dapp-ui/index.html"
+  },
+  {
+    "revision": "fc93186a1aa52dc5f610",
+    "url": "/dapp-ui/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "993a253fc8a3826f3752",
+    "url": "/dapp-ui/static/js/2.59d6a252.chunk.js"
+  },
+  {
+    "revision": "fc93186a1aa52dc5f610",
+    "url": "/dapp-ui/static/js/main.93c395cd.chunk.js"
+  },
+  {
+    "revision": "55b16d83211b1d6a17f9",
+    "url": "/dapp-ui/static/js/runtime~main.00a6935e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mtheory7/dapp-ui/precache-manifest.445a74636cda503abb6eac9c783b2aeb.js"
+  "/dapp-ui/precache-manifest.93ad97ded8da2fb8b9e29d9a147f0777.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mtheory7/dapp-ui/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dapp-ui/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
