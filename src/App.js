7f58b448ff1e44124455fd50bac8464dc8ad713d
Michael Stuhlman <mstuhlman@configureone.com>
2019-09-18 15:08:02 -0500

    Adding project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
+  const [account, setAccount] = useState("");
+  window.ethereum.enable().then(setAccount);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant="outline-primary">{account}</Button>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
