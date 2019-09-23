52f2f4a51628aa5c842172a6b763f529af12940a
Michael Stuhlman <mstuhlman@configureone.com>
2019-09-23 15:02:58 -0500

    Progress. Able to submit transaction for approval now

--- src/App.js
+++ src/App.js
@@ -5,10 +5,25 @@ import './App.css';
 function App() {
   const [account, setAccount] = useState("");
   window.ethereum.enable().then(setAccount);
+
   return (
     <div className="App">
       <header className="App-header">
-        <Button variant="outline-primary">{account}</Button>
+        <Button variant="outline-primary"
+          onClick={() => {
+            console.log(account[0]);
+            window.web3.eth.sendTransaction({
+              to:"0x76534516B97564626D191aB72aFbf056B9421e64",
+              from:account[0],
+              value:window.web3.toWei(0.01, "ether"),
+              gasPrice:"20000000000",
+              gasLimit:"21000"
+            }, function(err, transactionHash) {
+              if (!err)
+                console.log(transactionHash);
+            });
+          }}
+        >{account}</Button>
       </header>
     </div>
   );
